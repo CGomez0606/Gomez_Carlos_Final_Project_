b82c3a77b1b15f64bafc458d97644c6cd5bb3199
CGomez0606 <142941932+CGomez0606@users.noreply.github.com>
2023-12-01 09:04:49 -0800

    First Push

---
+++ main.py
@@ -0,0 +1,8 @@
+# This file was created by Carlos Gomez
+
+'''
+Make a game called Bellarun that has things that hurt and heal you
+obstacles to make it harder
+make it to the finish line
+use libraries to make mobs and more
+'''
